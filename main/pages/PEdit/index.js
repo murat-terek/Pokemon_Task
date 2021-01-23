60ee38deb108b744357d9c3e77d93cb04a3dd599
Murat Schumahua <murat-terek@yandex.ru>
2021-01-23 14:16:59 +0300

    Add Edit Delete actions

--- main/pages/PEdit/index.js
+++ main/pages/PEdit/index.js
@@ -64,7 +64,7 @@ export default observer(function PEdit () {
 
   const handleSave = () => {
     if (!pockemon) {
-      $pockemon.add({
+      $pockemon.addSelf({
         name,
         url,
         type,
--- model/PockemonModel.js
+++ model/PockemonModel.js
@@ -1,9 +1,9 @@
 import { BaseModel } from 'startupjs/orm'
 
 export default class PockemonModel extends BaseModel {
-  async add ({ name, url, type, abilities, description }) {
+  async addSelf({ name, url, type, abilities, description }) {
     await this.root.add(this.getCollection(), {
-      id: this.getId(),
+      id: this.id(),
       name,
       url,
       type,
